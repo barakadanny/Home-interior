3e496726235003e58cad4b55c8575a142fb00beb
barakadanny <barakadan421@gmail.com>
2024-10-22 10:05:47 +0100

    Update app js component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Loader2 } from "lucide-react";
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-500">
         Hello world!
       </h1>
+      <Button disabled size="lg">
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        Please wait
+      </Button>
     </>
   );
 }
