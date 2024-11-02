c5e25f09ca6b40f6eae395dcf7124c688b54f2fa
barakadanny <barakadan421@gmail.com>
2024-11-02 14:17:39 +0100

    Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Lightbulb } from "lucide-react";
 
 function Logo() {
   return (
-    <div>
-      <h1>Logo</h1>
-    </div>
+    <Link
+      to="/"
+      className="hidden lg:flex justify-center items-center bg-primary p-2 rounded-lg text-white "
+    >
+      <Lightbulb className="w-5 h-5" />
+    </Link>
   );
 }
 
