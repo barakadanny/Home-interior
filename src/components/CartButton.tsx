26b7dc8833a974f18619a276012a07b1e4a54940
barakadanny <barakadan421@gmail.com>
2024-11-06 10:36:52 +0100

    CartButton updated

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import { Button } from "./ui/button";
+import { ShoppingCart } from "lucide-react";
+import { Link } from "react-router-dom";
 
 function CartButton() {
+  const numItemsInCart = 5;
   return (
-    <div>
-      <h1>CartButton</h1>
-    </div>
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link to="/cart">
+        <ShoppingCart />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
   );
 }
 
