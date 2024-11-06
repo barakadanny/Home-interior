ed894b3ecf2711866103db32e62224c746b0e465
barakadanny <barakadan421@gmail.com>
2024-11-06 13:04:44 +0100

    Handle unexisting pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    errorElement: <Error />,
 
     children: [
       {
@@ -49,10 +50,12 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+    errorElement: <Error />,
   },
   {
     path: "/register",
     element: <Register />,
+    errorElement: <Error />,
   },
 ]);
 
