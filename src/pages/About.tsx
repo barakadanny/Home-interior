9ba5f479ac9ae683942c5c91670aefff6d96462b
barakadanny <barakadan421@gmail.com>
2024-11-12 11:34:07 +0100

    about page init

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-
 function About() {
   return (
-    <div>
-      <h1>About page</h1>
-    </div>
+    <section>
+      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-tight sm:text-6xl ">
+        Why
+        <span className="bg-primary py-2 px-4 rounded-lg  tracking-widest text-white">
+          Home Interior?
+        </span>
+      </h1>
+
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto ">
+        Welcome to Home Interior. We’re a team of people who love well-made,
+        thoughtfully designed furniture that makes home feel…well, more like
+        home. Every piece we offer is selected with real homes and real people
+        in mind—designed to bring comfort, style, and function to your spaces
+        without sacrificing quality or affordability.
+      </p>
+    </section>
   );
 }
 
