b95c2d392f525a2c1fe3a2ae9d7c42887ad3f8fb
barakadanny <barakadan421@gmail.com>
2024-11-16 11:53:51 +0100

    import loader /loader

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ import {
 } from "./pages";
 import ErrorElement from "./components/ErrorElement";
 
+import { loader as landingLoader } from "./pages/Landing";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -24,6 +26,7 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
+        loader: landingLoader,
       },
       {
         path: "products",
