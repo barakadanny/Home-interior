bfe2fa2af3dedf6786cbd4f26d756c528d2c8772
barakadanny <barakadan421@gmail.com>
2024-11-16 11:52:50 +0100

    Loader landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import { Hero, FeaturedProducts } from "@/components";
+import { customFetch, type ProductsResponse } from "@/utils";
+import { useLoaderData, type LoaderFunction } from "react-router-dom";
+
+const url = "/products?featured=true";
+
+export const loader: LoaderFunction = async () => {
+  console.log("This is running 1");
+  const response = await customFetch<ProductsResponse>(url);
+  console.log("This is running 2");
+  console.log(response);
+  return { ...response.data };
+};
 
 function Landing() {
+  const result = useLoaderData() as ProductsResponse;
+  console.log(result);
   return (
     <div>
-      <h1>Landing page</h1>
+      <Hero />
+      <FeaturedProducts />
     </div>
   );
 }
