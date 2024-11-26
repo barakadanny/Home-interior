f774cc384472e58aac1c81505f95101aca116d70
barakadanny <barakadan421@gmail.com>
2024-11-26 15:27:55 +0200

    FeaturedProducts updated

--- src/components/FeaturedProducts.tsx
+++ src/components/FeaturedProducts.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import ProductsGrid from "./ProductsGrid";
+import SectionTitle from "./SectionTitle";
 
 function FeaturedProducts() {
-  return <div>This is the featured products</div>;
+  return (
+    <section className="pt-24 ">
+      <SectionTitle text="featured products" />
+      <ProductsGrid />
+    </section>
+  );
 }
 
 export default FeaturedProducts;
