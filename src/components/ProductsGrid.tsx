301f9c41d42442d622f1091966265d33ed1c14b6
barakadanny <barakadan421@gmail.com>
2024-11-26 15:27:39 +0200

    Products Grid

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { Link, useLoaderData } from "react-router-dom";
+import { Card, CardContent } from "@/components/ui/card";
+import { formatAsDollars, ProductsResponse } from "@/utils";
+const ProductsGrid = () => {
+  const { data: products } = useLoaderData() as ProductsResponse;
 
-function ProductsGrid() {
-  return <div>this is the products grid</div>;
-}
+  return (
+    <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3 ">
+      {products.map((product) => {
+        const { title, price, image, company } = product.attributes;
+        const dollarsAmount = formatAsDollars(price);
 
+        return (
+          <Link to={`/products/${product.id}`} key={product.id}>
+            <Card>
+              <CardContent>
+                <img
+                  src={image}
+                  alt={title}
+                  className="h-64 md:h-48 w-full object-cover"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl font-semibold capitalize">{title}</h2>
+                  <span>{company}</span>
+                  <p className="text-primary font-light mt-2">
+                    {dollarsAmount}
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 export default ProductsGrid;
